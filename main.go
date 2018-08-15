a6dd4ca83ad93e4fc5c89b9fb9d84ae85c54c131
kevin <kevinalbertoorellana@gmail.com>
2018-08-15 17:49:33 -0600

    Prueba inicial para main

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "net/http"
+  "log"
+  "encoding/json"
+  "github.com/gorilla/mux"
+)
+
+type heartbeatResponse struct {
+  Status string `json:"status"`
+  Code int `json:"code"`
+}
+
+func main() {
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", heartbeat)
+  log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func heartbeat(w http.ResponseWriter, r *http.Request) {
+  json.NewEncoder(w).Encode(heartbeatResponse{Status: "OK", Code: 200})
+}
